36e77051dc13e9a91b731d384b50b1d3116572c7
Samyrperez <samyr.perez.ttc.485@unilibre.edu.co>
2024-12-09 19:19:46 -0500

    variables

---
+++ js/01-variables.js
@@ -0,0 +1,19 @@
+//var
+var helloWorld = "hello, Java Script"
+console.log(helloWorld)
+
+var helloWorld = "hello de nuevo Java Script"
+console.log(helloWorld)
+
+//let
+let helloWorld2 = "hello de nuevo Java Script"
+console.log(helloWorld2)
+
+
+
+//const
+//const es una variable que no puede ser reasignada, es inmutable
+const helloWorld3 = "hello de nuevo Java Script"
+console.log(helloWorld3)
+
+helloWorld3 = "hello de nuevo Java Script"

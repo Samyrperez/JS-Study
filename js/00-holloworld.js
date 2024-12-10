c49fa69c0643ba424420b4c633c76030fcd9ae9e
Samyrperez <samyr.perez.ttc.485@unilibre.edu.co>
2024-12-09 19:09:09 -0500

    hola mundo

---
+++ js/00-holloworld.js
@@ -0,0 +1,3 @@
+//console nos ayuda a interactuar con la terminal () los parentesis nos ayuda  allamar la
+console.log("Hola mundo")
+console.log('Hola mundo')
---
+++ js/conceptos.js
@@ -0,0 +1,4 @@
+//Motor V8 es el interprete encargado de interpretar JS y se encuentra en todos los navegadores.
+//Versión: JS puro, quiere decir JS tal cual se diseño.
+//PLAYGROUND- Esto para ejecutar nuestro codigo fuera de un navegador.
+//Para ver las buenas practivas buscamos en style javascript google (https://google.github.io/styleguide/jsguide.html)

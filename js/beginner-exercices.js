d7428448215dc04db0d9dbd11b9113fb66c8cda1
Samyrperez <samyr.perez.ttc.485@unilibre.edu.co>
2024-12-12 16:38:14 -0500

    Exercices

---
+++ js/beginner-exercices.js
@@ -0,0 +1,65 @@
+//1. Describe un comentario en la linea
+//Comentario
+
+//2. Escribe un comentario en varias lineas.
+/*Comentario de multiple
+line*/
+
+//3. Declara variables con valores asociados
+let nombre1 = "Samyr"
+let nombre2 = "David"
+const altura = 150.3
+const ancho = 150
+let esMayordeEdad = true
+let esHombre = true
+
+//4. Imprima por consola el valor de todas las variables
+console.log(esMayordeEdad)
+console.log(esHombre)
+console.log(nombre1)
+console.log(nombre2)
+console.log(altura)
+console.log(altura)
+
+//5. Imprime por consola el tipo de de todas las variables
+console.log(typeof nombre1)
+console.log(typeof nombre2)
+console.log(typeof altura)
+console.log(typeof ancho)
+console.log(typeof esMayordeEdad)
+console.log(typeof esHombre)
+
+//6. A continuación, modifica los valores de las variables por otros del mismo tipo
+nombre1= "Sam"
+console.log(nombre1)
+nombre2 = "Da"
+altura = 100
+ancho = 120
+esMayordeEdad = false
+esHombre = false
+
+//7. A continuación, modifica los valores de las variables por otros de distinto tipo
+nombre1= true
+console.log(nombre1)
+nombre2 = false
+altura = "100"
+ancho = "120"
+esMayordeEdad = 18
+esHombre = "maculino"
+//8. Declara constantes con valores asociados a todos los tipos de datos prmitivos
+//String
+const nombre = "Samyr"
+//Number
+const PI = 3.14
+const entero = 10
+//Boolean
+const bienvenido = true
+const mentiras = false
+//Null
+const edad = null
+//Symbol
+const mySymbol = Symbol("Simbolo")
+//BigInt
+const granEntero = 11122222222222333333333365458545874545212552122
+
+//10. Comenta las lineas que produzcan algún tipo de error al ejecutarse.

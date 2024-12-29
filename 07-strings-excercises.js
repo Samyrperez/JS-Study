6b06f3758e2637528456993cbb7f8c1fc98b954b
Samyrperez <samyr.perez.ttc.485@unilibre.edu.co>
2024-12-28 22:01:03 -0500

    Exercices string

---
+++ 07-strings-excercises.js
@@ -0,0 +1,19 @@
+// 1. Concatena dos cadenas de texto.
+
+// 2. Muestra al longitud de una cadena de texto.
+
+// 3. Muestra el primer y ultimo carcter de un string
+
+// 4. Convierte a mayusculas y minisculas un string
+
+// 5. Crea una cadena de texto en varias lineas.
+
+// 6. Interpola el valor de una variable string
+
+// 7. Reemplaza todos los espacios en blanco de un string por guiones.
+
+// 8. Comprueba si una cadena de etexto contiene una palabra concreta 
+
+// 9. Comprueba si dos string son iguales.
+
+// 10. Comprueba si dos string tiene la misma longitud.

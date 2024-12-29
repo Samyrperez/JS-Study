3b47e1e3569d75e3a6be4440411763b346f23eaa
Samyrperez <samyr.perez.ttc.485@unilibre.edu.co>
2024-12-28 22:09:39 -0500

    Conditional excercices

---
+++ 09-conditional-exercises.js
@@ -0,0 +1,23 @@
+//Conditional Excercices
+//if, else, else if, ternarias
+
+// 1. Imprime por ocnsola tu nombre si una variable toma tu su valor
+
+// 2. Imprime por consola un mensaje si el usuario y la contraseña coinciden con unos establecidos
+
+// 3. Verifica si un numero es positivo, negativo o cero e imprime un mensaje
+
+// 4. Verifica si una persona puede votar o no ( Mayor o igual a 18) e indica cuántos años le faltan
+
+// 5. Usa el operador ternario para asignar el valor "adulto" o "menor" a una variable dependiendo de la edad
+
+// 6. Muestra en que estación del año nos encontramos dependiendo del  valor de una variable "mes"
+
+// 7. Muestra el número de días que tiene un mes dependiendo de la variable del ejercicio anterior switch
+
+// 8. Usa un switch para imprimir un mensaje de saludo diferente dependiendo del idioma
+
+// 9. Usa un switch para hacer de nuevo el ejercicio 6
+
+// 10. Usa un switch para hacer de nuevo el ejercicio 7
+
